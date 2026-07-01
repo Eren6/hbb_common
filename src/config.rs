c648646f76cc478b226f3b94a0962ea6d467ed2e
Eren6 <511210003@qq.com>
2026-07-01 09:43:05 +0800

    配置修改

--- src/config.rs
+++ src/config.rs
@@ -121,7 +121,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const PUBLIC_RS_PUB_KEY: &str = "zZ7JoxSxYfaWB6FEfX1X7EmtWtNnHy22AA8N019V6fs=";
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {Some(key) if !key.is_empty() => key,
   _ => PUBLIC_RS_PUB_KEY,
 };
